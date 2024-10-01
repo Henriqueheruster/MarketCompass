aa54ac8f3d39cdb450a00ea93133d48d39366764
Henriqueheruster <henriquezxt@gmail.com>
2024-10-01 20:51:42 -0300

    Começando com olá

--- app/src/main/java/com/example/marketcompass/MainActivity.kt
+++ app/src/main/java/com/example/marketcompass/MainActivity.kt
@@ -1,47 +1,13 @@
 package com.example.marketcompass
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.marketcompass.ui.theme.MarketCompassTheme
+import com.example.marketcompass.R.layout.activity_main
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MarketCompassTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(activity_main)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MarketCompassTheme {
-        Greeting("Android")
-    }
 }
