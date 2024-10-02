4319a976ac21b71b2bbf682f5d45082670169741
android <henriquezxt@gmail.com>
2024-10-02 20:40:29 -0300

    Adicionando as duas primeiras telas

--- app/src/main/java/com/example/marketcompass/Tela_comecar.kt
+++ app/src/main/java/com/example/marketcompass/Tela_comecar.kt
@@ -2,14 +2,11 @@ package com.example.marketcompass
 
 import android.app.Activity
 import android.os.Bundle
-import com.example.marketcompass.R.layout.activity_main
+import com.example.marketcompass.R.layout.tela_comecar
 
-class MainActivity : Activity(){
+class Tela_comecar:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(activity_main)
-
-
+        setContentView(tela_comecar)
     }
-
 }
