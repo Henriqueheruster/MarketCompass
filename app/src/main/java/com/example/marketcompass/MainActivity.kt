02301d7d10c639a208ece308256f6682b5be12ac
Emanuel Feliciano Urbano <62392361+EmanuelUrbano@users.noreply.github.com>
2024-10-09 15:18:31 -0300

    'a'

--- app/src/main/java/com/example/marketcompass/MainActivity.kt
+++ app/src/main/java/com/example/marketcompass/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.marketcompass
 import android.app.Activity
 import android.os.Bundle
 import com.example.marketcompass.R.layout.activity_main
+import com.example.marketcompass.R.layout.tela_comecar
 
 class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,7 @@ class MainActivity : Activity(){
 
 
     }
+
+
 
 }
--- app/src/main/java/com/example/marketcompass/TelaComecar.kt
+++ app/src/main/java/com/example/marketcompass/TelaComecar.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import com.example.marketcompass.R.layout.tela_comecar
 
-class Tela_comecar:Activity() {
+class TelaComecar:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(tela_comecar)
