47d002067b8823b50e9414332f258894c8309b7d
Henriqueheruster <henriquezxt@gmail.com>
2024-11-06 20:35:06 -0300

    deu certo de pegar os dados da API

--- app/src/main/java/com/example/marketcompass/TelaMercado.kt
+++ app/src/main/java/com/example/marketcompass/TelaMercado.kt
@@ -3,13 +3,16 @@ package com.example.marketcompass
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import com.example.marketcompass.R.layout.tela_mercados
 import com.example.marketcompass.util.NetworkUtils
 import com.example.marketcompass.util.api.Endpoint
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import retrofit2.Call
+import retrofit2.Callback
 
 import retrofit2.Response
 
@@ -17,39 +20,32 @@ class TelaMercado:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(tela_mercados)
+        getMercado()
         val btnDoMercado : CardView = findViewById(R.id.mercado1)
-        getMercado();
         btnDoMercado.setOnClickListener{
-
             val intencao : Intent = Intent(this, TelaLista::class.java)
-
             startActivity(intencao)
         }
     }
 
     fun getMercado(){
-        val retrofitClient = NetworkUtils.getRetrofitInstance("http://localhost:8181")
+        val retrofitClient = NetworkUtils.getRetrofitInstance("http://10.0.2.2:8181/")
         val endpoint = retrofitClient.create(Endpoint::class.java)
 
+        endpoint.getMercado().enqueue(object : Callback<JsonArray>{
 
-
-        endpoint.getMercado("").enqueue(object :
-
-        retrofit2.Callback<JsonObject>{
-
-            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+            override fun onResponse(call: Call<JsonArray>, response: Response<JsonArray>) {
                 val data = mutableListOf<String>()
 
-                response.body()?.keySet()?.iterator()?.forEach{
-                    data.add(it)
-                }
 
-                println(data.count())
+                Log.d("teste mercados", "Dados recebidos da API: ${data.joinToString(", ")}")
             }
 
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                print("Não foi encontrado!")
+            override fun onFailure(call: Call<JsonArray>, t: Throwable) {
+                Log.e("Erro Retrofit", "Falha na requisição: ${t.message}")
+                Toast.makeText(this@TelaMercado, "Não foi encontrado", Toast.LENGTH_SHORT).show()
             }
         })
+        endpoint.getMercado()
     }
 }
--- app/src/main/java/com/example/marketcompass/util/api/Endpoint.kt
+++ app/src/main/java/com/example/marketcompass/util/api/Endpoint.kt
@@ -1,14 +1,15 @@
 package com.example.marketcompass.util.api
 
 import com.example.marketcompass.dados.mercados
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface Endpoint {
-    @GET("/mercadosJson/{from}")
-    fun getMercado(@Path(value = "from", encoded = true)from:String) : Call<JsonObject>
+    @GET("mercadosJson")
+    fun getMercado() : Call<JsonArray>
 
     @GET("/produtosMercado/{id}")
     fun getCurrentRate(@Path(value = "id", encoded = true)from:String) : Call<mercados>
